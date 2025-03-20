f522e40461f2e04ec5a275677f1881586ac1b318
unknown <maite.perez@lacoloniamanangent.com>
2025-03-20 14:13:35 -0400

    feat: refactor ActionButton to be responsive

--- src/components/ActionButton.jsx
+++ src/components/ActionButton.jsx
@@ -1,5 +1,7 @@
 import React from "react";
-import { Tooltip, Button } from "@mui/material";
+import PropTypes from "prop-types";
+import { Tooltip, Button, useMediaQuery } from "@mui/material";
+import { useTheme } from "@mui/system";
 
 export const ActionButton = ({
   title,
@@ -8,11 +10,33 @@ export const ActionButton = ({
   onClick,
   variant = "outlined",
 }) => {
+  const theme = useTheme();
+  const isSmallScreen = useMediaQuery(theme.breakpoints.down("sm"));
+
   return (
     <Tooltip title={title}>
-      <Button variant={variant} startIcon={icon} onClick={onClick}>
-        {label}
+      <Button
+        variant={variant}
+        startIcon={isSmallScreen ? null : icon}
+        onClick={onClick}
+        sx={{
+          minWidth: isSmallScreen ? "40px" : "auto",
+          padding: isSmallScreen ? "6px" : "8px 16px",
+          display: "flex",
+          justifyContent: "center",
+          alignItems: "center",
+        }}
+      >
+        {isSmallScreen ? icon : label}
       </Button>
     </Tooltip>
   );
+};
+
+ActionButton.propTypes = {
+  title: PropTypes.string.isRequired,
+  label: PropTypes.string,
+  icon: PropTypes.node,
+  onClick: PropTypes.func.isRequired,
+  variant: PropTypes.oneOf(["text", "outlined", "contained"]),
 };
